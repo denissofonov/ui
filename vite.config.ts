aa6339b8d4ffb299f5c885c02194cbceaf19d4a1
dsofonov <dsofonov@azbukavkusa.ru>
2023-11-19 14:57:43 +0300

    fix process.env

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,30 @@
 import { fileURLToPath, URL } from 'node:url'
 
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  },
-  css: {
-    preprocessorOptions: {
-      sass: {
-        additionalData: `@import "./src/assets/styles/global.sass"`,
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+
+  return {
+    define: {
+      'process.env.SOME_KEY': JSON.stringify(env.SOME_KEY)
+    },
+    plugins: [
+      vue(),
+    ],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
+      }
+    },
+    css: {
+      preprocessorOptions: {
+        sass: {
+          additionalData: `@import "./src/assets/styles/global.sass"`,
+        },
       },
     },
-  },
+  }
 })
